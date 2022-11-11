b4f5581d36a7f905eba27462e2081920a3889995
Mohammed <melgadi.timwi@gmail.com>
2022-11-11 22:56:18 +0100

    first commit - header and code style structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/layout/Header";
+import React from "react";
+
+const FoxRoutes = 
+  <Route path="/"
+    element={
+        <Header />
+    }
+  >
+    <Route path="/home" element={<Home />} />
+  </Route>;
+
+const router = createBrowserRouter(createRoutesFromElements(FoxRoutes));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="container-fluid">
+        <nav className="navbar navbar-expand-lg">
+            <Link className="navbar-brand" href="#"><img src="/assets/images/Logo_foxchange.png" alt=""/></Link>
+            <form className="form-inline my-2 my-lg-0 ">
+                <input className="form-control mr-sm-2" type="search"
+                    placeholder="Search NFTs, collections, artist and genres..." aria-label="Search" id="formSearch"/>
+
+            </form>
+            <button className="navbar-toggler navbar-dark" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/">Explorer <span className="sr-only">(current)</span></Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Stats</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Resource</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Create</Link>
+                    </li>
+                </ul>
+
+            </div>
+            <ul id="buttonIcon">
+                <li><img src="/assets/icon-white-user.png" alt=""/></li>
+                <li><img src="/assets/icon-white-settings.png" alt=""/></li>
+                <li><img src="/assets/icon-white-wallet.png" alt=""/></li>
+                
+            </ul>
+           <button id="signUpButton">Connect Wallect</button>
+        </nav>
+    </header>
+  );
+};
+
+export default Header;
