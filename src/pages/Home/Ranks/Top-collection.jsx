2ea886e0285fc92942184146815913fe727b984d
Martin Orlej <xorlej@stuba.sk>
2022-12-02 20:43:51 +0100

    trending and top collections on homepage now based on api

--- src/pages/Home/Ranks/Top-collection.jsx
+++ src/pages/Home/Ranks/Top-collection.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
+import { getCollections } from "../../../api/collectionApi";
 import RankItem from "../../../components/RankItem";
 
-const TopCollections = () => {
+const TopCollections = (props) => {
+
+  const [items, setItems] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setItems(getCollections(null,'TOP'))
+    setLoading(false)
+  }, [])
+
   return (
     <>
-      <RankItem elm = {"- top"}/>
-      <RankItem elm = {"- top"}/>
-      <RankItem elm = {"- top"}/>
+      {
+        loading && items.length === 0
+        ?
+        <div>Loading...</div>
+        : 
+        items.map((item,index) => {
+          return <RankItem key={index} itemData={item} />
+        })
+      }
     </>
   );
 };
