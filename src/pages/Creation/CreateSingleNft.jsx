cc798da905f98a1c5174b6c5a70b391497a40042
Martin Orlej <xorlej@stuba.sk>
2022-12-14 14:00:39 +0100

    create nft popups logic

--- src/pages/Creation/CreateSingleNft.jsx
+++ src/pages/Creation/CreateSingleNft.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
-import { toast} from "react-toastify";
+import { toast } from "react-toastify";
 import Spinner from "../../components/Spinner";
 import { createNftDB } from "../../services/createNFT";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { selectConnectedWallet, selectToken } from "../../redux/userReducer";
-import PopupContainerWrapper from "../../components/popups/popups";
+import PopupContainerWrapper, {
+  CreateNFTPopup,
+} from "../../components/popups/popups";
 
 const CreateSingleNft = () => {
-
   const [imageUpload, setImageUpload] = useState(null);
   const [imageData, setImageData] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [popupStatus, setPopupStatus] = useState(false);
+  const [popupStatus, setPopupStatus] = useState("properties");
 
   const [nftData, setNftData] = useState({
     artworkName: "",
@@ -24,34 +25,55 @@ const CreateSingleNft = () => {
     properties: [],
     levels: [],
     stats: [],
-  })
+  });
 
   const handleChange = (e) => {
-    if(e.target.name === "upload") {
- 
+    if (e.target.name === "upload") {
       const reader = new FileReader();
       reader.onloadend = () => {
         setImageUpload(URL.createObjectURL(e.target.files[0]));
         setImageData(reader.result);
-      }
+      };
       reader.readAsDataURL(e.target.files[0]);
-    }
-    else{
+    } else {
       setNftData({
         ...nftData,
-        [e.target.name]: e.target.value
-      })
+        [e.target.name]: e.target.value,
+      });
     }
-    
-  }
+  };
+
+  const handleAddToArray = (arrayTarget, objectToInsert) => {
+    // check if object is already in array
+    const objectExists = nftData[arrayTarget].find(
+      (item) => item.name === objectToInsert.name
+    );
+    if (objectExists) {
+      toast.error("Object already exists");
+    }
+    // if not, add object to array
+    else {
+      setNftData({
+        ...nftData,
+        [arrayTarget]: [...nftData[arrayTarget], objectToInsert],
+      });
+    }
+  };
+
+  const handleRemoveFromArray = (arrayTarget, objectToRemove) => {
+    const newArray = nftData[arrayTarget].filter(
+      (item) => item.name !== objectToRemove.name
+    );
+    setNftData({
+      ...nftData,
+      [arrayTarget]: newArray,
+    });
+  };
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
     console.log(nftData);
-
-
-
 
     setLoading(false);
 
@@ -76,7 +98,7 @@ const CreateSingleNft = () => {
       }
     }
     */
-  }
+  };
 
   // const handleImageUpload = (e) => {
   //   const file = e.target.files[0];
@@ -89,32 +111,38 @@ const CreateSingleNft = () => {
   //       setImageData(reader.result);
   //     }
   //     reader.readAsDataURL(file);
-      
+
   //   }
-   
 
-    
   // }
-
-
 
   return (
     <section id="createCollection" className="my-2">
-      <PopupContainerWrapper values={nftData} updateAction={handleChange} popupType={popupStatus} popupCloseAction={setPopupStatus} />
+      <CreateNFTPopup
+        values={nftData}
+        updateAction={handleChange}
+        popupType={popupStatus}
+        popupTitle={popupStatus}
+        popupCloseAction={setPopupStatus}
+        handleAddToArray={handleAddToArray}
+        handleRemoveFromArray={handleRemoveFromArray}
+        nftData={nftData}
+      />
       <img src="/assets/images/Background.jpg" id="layer" alt="" />
       <h3 className="text-center mb-5">Mint Your NFT</h3>
       <div className="container pt-3 ">
-        {
-          loading
-          ?
+        {loading ? (
           <div className="processing processingMargin">
             <Spinner />
             <h2>Processing</h2>
           </div>
-          :
-          <form onSubmit={handleFormSubmit}  className="row text-center">
+        ) : (
+          <form onSubmit={handleFormSubmit} className="row text-center">
             <div className="col-md-4 col-sm-12 mb-5">
-              <label id="upload" className={`${imageUpload ? 'uploaded' : null}`} >
+              <label
+                id="upload"
+                className={`${imageUpload ? "uploaded" : null}`}
+              >
                 <img src={imageUpload} alt="" />
                 <div>
                   <svg
@@ -131,7 +159,13 @@ const CreateSingleNft = () => {
                     />
                     <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
                   </svg>
-                  <input required type="file" name="upload" id="uploadImage"  onChange={(e) => handleChange(e)} />
+                  <input
+                    required
+                    type="file"
+                    name="upload"
+                    id="uploadImage"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <p>Upload Image</p>
                   <span>Max 8 Mb.</span>
                 </div>
@@ -196,76 +230,108 @@ const CreateSingleNft = () => {
                 <div className="form-row mb-4">
                   <div className="form-group col-md-6">
                     <label htmlFor="inputState">Rights Level</label>
-                    <select id="inputState" className="form-control" defaultValue="Level1" name="rightsLevel" onChange={(e) => handleChange(e)} required>
-                      <option value="Level1" selected>Level1</option>
+                    <select
+                      id="inputState"
+                      className="form-control"
+                      defaultValue="Level1"
+                      name="rightsLevel"
+                      onChange={(e) => handleChange(e)}
+                      required
+                    >
+                      <option value="Level1" selected>
+                        Level1
+                      </option>
                     </select>
                   </div>
                   <div className="form-group col-md-6">
                     <label htmlFor="inputState">Rights Duration</label>
-                    <select id="inputState" className="form-control" defaultValue="10 Years" name="rightsDurations" onChange={(e) => handleChange(e)} required>
-                      <option value="10 Years" selected>10 Years</option>
+                    <select
+                      id="inputState"
+                      className="form-control"
+                      defaultValue="10 Years"
+                      name="rightsDurations"
+                      onChange={(e) => handleChange(e)}
+                      required
+                    >
+                      <option value="10 Years" selected>
+                        10 Years
+                      </option>
                     </select>
                   </div>
                 </div>
                 <h3 className="mt-12 mb-1">Properties</h3>
-                <p className="text-white mb-3 ">Textual traits that show up as rectangles</p>
+                <p className="text-white mb-3 ">
+                  Textual traits that show up as rectangles
+                </p>
                 <div className="form-row mb-4 mt-4 mb-5">
                   <div className="properties-row">
-                    {
-                      nftData.properties.map((property, index) => {
-                        return (
-                          <div className="property" key={index}>
-                              <p className="property_name">{property.name}</p>
-                              <p className="property_value">{property.value}</p>
-                          </div>
-                        )
-                      })
-                    }
-                    
-                    <div className="property propertyAdd" onClick={() => setPopupStatus("properties")}>
+                    {nftData.properties.map((property, index) => {
+                      return (
+                        <div className="property" key={index}>
+                          <p className="property_name">{property.name}</p>
+                          <p className="property_value">{property.value}</p>
+                        </div>
+                      );
+                    })}
+
+                    <div
+                      className="property propertyAdd"
+                      onClick={() => setPopupStatus("properties")}
+                    >
                       <p>+</p>
                     </div>
                   </div>
                 </div>
                 <h3 className="mt-12 mb-1">Levels</h3>
-                <p className="text-white mb-3 ">Numerical traits that show as a progress bar</p>
+                <p className="text-white mb-3 ">
+                  Numerical traits that show as a progress bar
+                </p>
                 <div className="levels mb-4 mt-4 mb-5">
-                  
-                  {
-                    nftData.levels.map((level, index) => {
-                      return(
-                        <div className="level" key={index}>
-                          <div className="py-2 levelHead d-flex align-items-center justify-content-between">
-                            <h4>{level.name}</h4>
-                            <p className="mb-0">
-                              {level.value} of {level.max}
-                            </p>
-                          </div>
-                          <progress className="d-block w-full" value="2" max="5" />
+                  {nftData.levels.map((level, index) => {
+                    return (
+                      <div className="level" key={index}>
+                        <div className="py-2 levelHead d-flex align-items-center justify-content-between">
+                          <h4>{level.name}</h4>
+                          <p className="mb-0">
+                            {level.valueMin} of {level.valueMax}
+                          </p>
                         </div>
-                      )
-                    })
-                  }
-                  <div className="property propertyAdd" onClick={() => setPopupStatus("levels")}>
-                      <p>+</p>
+                        <progress
+                          className="d-block w-full"
+                          value={level.valueMin}
+                          max={level.valueMax}
+                        />
+                      </div>
+                    );
+                  })}
+                  <div
+                    className="property propertyAdd"
+                    onClick={() => setPopupStatus("levels")}
+                  >
+                    <p>+</p>
                   </div>
                 </div>
                 <h3 className="mt-12 mb-1">Stats</h3>
-                <p className="text-white mb-3 ">Numerical traits that just show as numbers</p>
+                <p className="text-white mb-3 ">
+                  Numerical traits that just show as numbers
+                </p>
                 <div className="stats mb-4 mt-4 mb-5">
-                    {
-                      nftData.stats.map((stat, index) => {
-                        return(
-                          <div className="stat" key={index}>
-                            <h4 >Stat Name</h4>
-                            <p className="mb-0">0 of 5</p>
-                          </div>
-                        )
-                      })
-                    }
-                    <div className="property propertyAdd" onClick={() => setPopupStatus("stats")}>
-                      <p>+</p>
-                    </div>
+                  {nftData.stats.map((stat, index) => {
+                    return (
+                      <div className="stat" key={index}>
+                        <h4>{stat.name}</h4>
+                        <p className="mb-0">
+                          {stat.valueMin} of {stat.valueMax}
+                        </p>
+                      </div>
+                    );
+                  })}
+                  <div
+                    className="property propertyAdd"
+                    onClick={() => setPopupStatus("stats")}
+                  >
+                    <p>+</p>
+                  </div>
                 </div>
 
                 <button type="submit" className="btn">
@@ -274,8 +340,7 @@ const CreateSingleNft = () => {
               </div>
             </div>
           </form>
-        }
-        
+        )}
       </div>
     </section>
   );
