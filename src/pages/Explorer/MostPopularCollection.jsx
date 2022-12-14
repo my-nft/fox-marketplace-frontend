45bed33c3cc235987be78a03fd248266419a6a9c
Martin Orlej <xorlej@stuba.sk>
2022-12-14 12:50:24 +0100

    minor modifications and refactoring

--- src/pages/Explorer/MostPopularCollection.jsx
+++ src/pages/Explorer/MostPopularCollection.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Slider from "react-slick";
 import { getCollections } from "../../api/collectionApi";
 import ExplorePopularCollectionItem from "../../components/ExplorePopularCollectionItem";
@@ -6,6 +7,19 @@ import { putSliderIcons, settings } from "../Home/Utils";
 
 const MostPopularCollection = ({ collections }) => {
   const [popularCollections, setPopularCollections] = useState([]);
+
+  const [isDragging, setIsDragging] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLinkClick = (link) => {
+    if (!isDragging) {
+      navigate(link);
+    }
+  };
+
+  useEffect(() => {
+    console.log("DRAGGING", isDragging);
+  }, [isDragging]);
 
   useEffect(() => {
     setPopularCollections(collections);
@@ -17,7 +31,7 @@ const MostPopularCollection = ({ collections }) => {
       <img src="./assets/images/Background.jpg" id="layer" />
       <h3 className="mb-2 mt-2 text-center">Most popular collection</h3>
       {popularCollections.length > 0 ? (
-        <Slider {...settings} className='explorer-slick'>
+        <Slider {...settings} className="explorer-slick" onInit>
           {popularCollections.map((item, index) => {
             return <ExplorePopularCollectionItem key={index} itemData={item} />;
           })}
