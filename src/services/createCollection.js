beeaa103e2d165939b38cfd63620b7ade3058ae9
Martin Orlej <xorlej@stuba.sk>
2022-12-25 23:05:53 +0100

    fixed nfts wrapper issue, added margin between list button, modified traits

--- src/services/createCollection.js
+++ src/services/createCollection.js
@@ -2,54 +2,48 @@ import apiUrl from "../config/api";
 import methods from "../config/axiosConfig";
 
 export const createNewCollection = async (data) => {
-    const { 
-        collectionTokenName, 
-        collectionTokenSymbol, 
-        collectionAddress,
-        walletAddress,
-        artistName, 
-        email, 
-        description, 
-        rightsLevel,
-        rightsDuration, 
-        upload
-    } = data;
+  const {
+    collectionTokenName,
+    collectionTokenSymbol,
+    collectionAddress,
+    walletAddress,
+    artistName,
+    email,
+    description,
+    rightsLevel,
+    rightsDuration,
+    upload,
+  } = data;
 
-    console.log("UPLOAD: " + JSON.stringify(upload))
+  console.log("UPLOAD: " + JSON.stringify(upload));
 
-    const outputDataMap = {
-        name: collectionTokenName,
-        description,
-        artistName,
-        ownerAddress: walletAddress,
-        image: JSON.stringify(upload),
+  const outputDataMap = {
+    name: collectionTokenName,
+    description,
+    artistName,
+    ownerAddress: walletAddress,
+    image: upload,
+  };
 
-    }
-   
+  console.log("*******************************************");
+  console.log("CREATING NEW COLLECTION");
+  console.log("OUTPUT DATA MAP: " + JSON.stringify(outputDataMap));
+  console.log("COLLECTION ADDRESS: " + collectionAddress);
 
-    console.log("*******************************************")
-    console.log("CREATING NEW COLLECTION");
-    console.log("OUTPUT DATA MAP: " + JSON.stringify(outputDataMap));
-    console.log("COLLECTION ADDRESS: " + collectionAddress);
-
-
-
-    // call createCollectionService to create new collection
-    methods.post(`${apiUrl}collections/${collectionAddress}`, outputDataMap, {
-        headers: {
-            'Content-Type': 'multipart/form-data',
-        }
+  // call createCollectionService to create new collection
+  methods
+    .post(`${apiUrl}collections/${collectionAddress}`, outputDataMap, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
     })
-    .then(res => {
-        console.log("COLLECTION CREATE RESPONSE:",res);
+    .then((res) => {
+      console.log("COLLECTION CREATE RESPONSE:", res);
     })
-    .catch(err => {
-        console.log("COLLECTION CREATE ERROR:",err);
+    .catch((err) => {
+      console.log("COLLECTION CREATE ERROR:", err);
     })
     .finally(() => {
-        return null;
-    })
-
-    
-
-}
+      return null;
+    });
+};
