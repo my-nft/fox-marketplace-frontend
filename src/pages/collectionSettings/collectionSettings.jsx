dcb138b1accab133f62818ec3e9203e1fd63e23f
Mohammed El Gadi <melgadi.timwi@gmail.com>
2022-12-31 00:51:15 +0100

    small correction

--- src/pages/collectionSettings/collectionSettings.jsx
+++ src/pages/collectionSettings/collectionSettings.jsx
@@ -41,7 +41,7 @@ const CollectionSettings = () => {
     setIsLoadingCollection(true);
     const response = await getCollectionByAddress(collectionAddress);
     const { data } = response;
-    const {collection} = response;
+    const {collection} = data;
     setCollectionDetails(collection);
     setImage(data.image);
     setBanner(data.banner);
