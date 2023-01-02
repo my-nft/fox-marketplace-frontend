5c8e0551fb0c1f1bd9804270cc9c227f4f9613f4
Martin Orlej <xorlej@stuba.sk>
2023-01-02 09:02:30 +0100

    removed dollar pricing

--- src/components/PopularCollectionItem.jsx
+++ src/components/PopularCollectionItem.jsx
@@ -1,25 +1,24 @@
 const PopularCollectionItem = (props) => {
+  const { itemData } = props;
 
-    const { itemData } = props;
-
-    return (
-        <div className="popularItems col-sm-6 col-md-3">
-          <img src={itemData.image} alt=""/>
-          <div className="nameItem">
-            <span className="name">{itemData.name}</span>
-            <span>
-              {itemData.likes}{" "}
-              <img
-                src="/assets/images/Iconmonstr-favorite-2-16.png"
-                style={{"width": "16px"}}
-                alt=""
-              />
-            </span>
-          </div>
-          <p>${itemData.lowestAsk} Lowest ask</p>
-          <p>{itemData.tokens}LAU</p>
-        </div>
-    );
+  return (
+    <div className="popularItems col-sm-6 col-md-3">
+      <img src={itemData.image} alt="" />
+      <div className="nameItem">
+        <span className="name">{itemData.name}</span>
+        <span>
+          {itemData.likes}{" "}
+          <img
+            src="/assets/images/Iconmonstr-favorite-2-16.png"
+            style={{ width: "16px" }}
+            alt=""
+          />
+        </span>
+      </div>
+      <p>{/* ${itemData.lowestAsk} Lowest ask */}</p>
+      <p>{itemData.tokens}LAU</p>
+    </div>
+  );
 };
 
 export default PopularCollectionItem;
--- src/components/nft/CardBody.jsx
+++ src/components/nft/CardBody.jsx
@@ -2,17 +2,25 @@ import { useSelector } from "react-redux";
 import { selectCurrentWallet } from "../../redux/userReducer";
 import { sameAddress } from "../../utils/walletUtils";
 
-const CardBody = ({ title, price, ownerAddress, priceDollar, bestOffer, onAcceptOffer = () => {}, children }) => {
-  
+const CardBody = ({
+  title,
+  price,
+  ownerAddress,
+  priceDollar,
+  bestOffer,
+  onAcceptOffer = () => {},
+  children,
+}) => {
   const currentWallet = useSelector(selectCurrentWallet);
-  
+
   return (
     <div className="card-body">
       <div className="card-text">
         <h3>{title}</h3>
         {price ? (
           <p id="price">
-            {price} <span id="priceDollar">${priceDollar}</span>
+            {price} FXG
+            {/* <span id="priceDollar">${priceDollar}</span> */}
           </p>
         ) : null}
 
@@ -20,16 +28,14 @@ const CardBody = ({ title, price, ownerAddress, priceDollar, bestOffer, onAccept
           <>
             <h5>Best offer</h5>
             <p id="price">
-              {bestOffer} <span id="priceDollar">${bestOffer}</span>
+              {bestOffer} FXG
+              {/* <span id="priceDollar">${bestOffer}</span> */}
             </p>
-            {
-              sameAddress(currentWallet, ownerAddress) ? (
-                <button id="makeOffer" className="btn" onClick={onAcceptOffer}>
-              Accept Offer
-            </button>
-              ) : null
-            }
-            
+            {sameAddress(currentWallet, ownerAddress) ? (
+              <button id="makeOffer" className="btn" onClick={onAcceptOffer}>
+                Accept Offer
+              </button>
+            ) : null}
           </>
         ) : null}
         {children}
