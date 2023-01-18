28add51ee04b4c90db4994eb08ba0602c8660479
Martin Orlej <xorlej@stuba.sk>
2023-01-18 14:03:35 +0100

    infinite scroll for nft details diagrams

--- src/components/nft/activity.jsx
+++ src/components/nft/activity.jsx
@@ -3,16 +3,30 @@ import InfoBoxWrapper from "./infoBoxWrapper";
 import { ReactComponent as ContentIcon } from "./../../assets/icons/content.svg";
 import Address from "./../Address";
 import { optimizeWalletAddress } from "../../utils/walletUtils";
-import { EVENT_ENUM } from "../../utils/foxConstantes";
+import { EVENT_BUY_LISTING, EVENT_ENUM } from "../../utils/foxConstantes";
 import Spinner from "../Spinner";
 import { dateToUserFriendlyValue } from "../datePicker/utils";
 import Transaction from "../Transaction";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useEffect, useState } from "react";
 
 const ItemActivity = ({ activity = [], isLoading }) => {
-  
+  const [dataForRender, setDataForRender] = useState(activity.slice(0, 8));
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    if (dataForRender.length >= activity.length) {
+      setHasMore(false);
+    }
+  }, [dataForRender]);
+
+  const updateAction = () => {
+    setDataForRender(activity.slice(0, dataForRender.length + 8));
+  };
+
   return (
     <InfoBoxWrapper title="Item Activity">
-      <div className="activity">
+      <div className="activity ">
         <div className="infoBoxGrid infoBoxHeader">
           <p>Event</p>
           <p>Price</p>
@@ -21,7 +35,46 @@ const ItemActivity = ({ activity = [], isLoading }) => {
           <p>Date</p>
           <p>Transaction</p>
         </div>
-        {activity.map((event, index) => {
+        {activity.length > 0 && (
+          <InfiniteScroll
+            dataLength={dataForRender.length}
+            next={updateAction}
+            loader={<Spinner />}
+            hasMore={hasMore}
+            height={400}
+          >
+            {dataForRender.map((event, index) => {
+              return (
+                <div className="infoBoxGrid infoRow" key={index}>
+                  <p>{EVENT_ENUM[event.event]}</p>
+                  <p>{event.price} FXG</p>
+                  <p>
+                    {event.fromAddress && (
+                      <Address address={event.fromAddress}>
+                        {optimizeWalletAddress(event.fromAddress)}
+                      </Address>
+                    )}
+                  </p>
+                  <p>
+                    {event.toAddress && (
+                      <Address address={event.toAddress}>
+                        {optimizeWalletAddress(event.toAddress)}
+                      </Address>
+                    )}
+                  </p>
+                  <p>{dateToUserFriendlyValue(event.date_event)}</p>
+                  <p>
+                    <Transaction address={event.transactionId}>
+                      {optimizeWalletAddress(event.transactionId)}
+                    </Transaction>
+                  </p>
+                </div>
+              );
+            })}
+          </InfiniteScroll>
+        )}
+
+        {/* {activity.map((event, index) => {
           return (
             <div className="infoBoxGrid infoRow" key={index}>
               <p>{EVENT_ENUM[event.event]}</p>
@@ -48,7 +101,7 @@ const ItemActivity = ({ activity = [], isLoading }) => {
               </p>
             </div>
           );
-        })}
+        })} */}
         {isLoading && (
           <Spinner>
             <p>Fetching Data</p>
--- src/components/nft/offers.jsx
+++ src/components/nft/offers.jsx
@@ -6,8 +6,23 @@ import Address from "./../Address";
 import Spinner from "../Spinner";
 import { dateToUserFriendlyValue } from "../datePicker/utils";
 import Transaction from "../Transaction";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useEffect, useState } from "react";
 
 const Offers = ({ itemExtra = [], isLoading }) => {
+  const [dataForRender, setDataForRender] = useState(itemExtra.slice(0, 8));
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    if (dataForRender.length >= itemExtra.length) {
+      setHasMore(false);
+    }
+  }, [dataForRender]);
+
+  const updateAction = () => {
+    setDataForRender(itemExtra.slice(0, dataForRender.length + 8));
+  };
+
   return (
     <InfoBoxWrapper title={"Offers"}>
       <div className="offers">
@@ -18,27 +33,40 @@ const Offers = ({ itemExtra = [], isLoading }) => {
           <p>From</p>
           <p>Transaction</p>
         </div>
-        {itemExtra.map((offer, index) => {
-          return (
-            <div className="infoBoxGrid infoRow" key={index}>
-              <p>{parseFloat(offer.price.toFixed(4))} FXG</p>
-              <p>${parseFloat((Number(offer.price) * FXG_PRICE).toFixed(4))}</p>
-              <p>{dateToUserFriendlyValue(offer.date_event)}</p>
-              <p>
-                {offer.fromAddress && (
-                  <Address address={offer.fromAddress}>
-                    {optimizeWalletAddress(offer.fromAddress)}
-                  </Address>
-                )}
-              </p>
-              <p>
-                <Transaction address={offer.transactionId}>
-                  {optimizeWalletAddress(offer.transactionId)}
-                </Transaction>
-              </p>
-            </div>
-          );
-        })}
+        {itemExtra.length > 0 && (
+          <InfiniteScroll
+            dataLength={dataForRender.length}
+            next={updateAction}
+            loader={<Spinner />}
+            hasMore={hasMore}
+            height={300}
+          >
+            {dataForRender.map((offer, index) => {
+              return (
+                <div className="infoBoxGrid infoRow" key={index}>
+                  <p>{parseFloat(offer.price.toFixed(4))} FXG</p>
+                  <p>
+                    ${parseFloat((Number(offer.price) * FXG_PRICE).toFixed(4))}
+                  </p>
+                  <p>{dateToUserFriendlyValue(offer.date_event)}</p>
+                  <p>
+                    {offer.fromAddress && (
+                      <Address address={offer.fromAddress}>
+                        {optimizeWalletAddress(offer.fromAddress)}
+                      </Address>
+                    )}
+                  </p>
+                  <p>
+                    <Transaction address={offer.transactionId}>
+                      {optimizeWalletAddress(offer.transactionId)}
+                    </Transaction>
+                  </p>
+                </div>
+              );
+            })}
+          </InfiniteScroll>
+        )}
+
         {isLoading && (
           <Spinner>
             <p>Fetching Data</p>
