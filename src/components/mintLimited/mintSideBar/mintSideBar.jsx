8e53268c25a0f5902fea8488a5fd8949096af287
Mohammed El Gadi <melgadi.timwi@gmail.com>
2023-02-02 12:46:31 +0100

    enable white listing

--- src/components/mintLimited/mintSideBar/mintSideBar.jsx
+++ src/components/mintLimited/mintSideBar/mintSideBar.jsx
@@ -5,36 +5,41 @@ import MintCounter from "../mintCounter/mintCounter";
 const MintSideBar = ({
   mintAction,
   mintingData = {},
-  handleMinting = () => {}
+  handleMinting = () => {},
 }) => {
-  const { mintingEnabled, name, mintFee } = mintingData;
+  const { mintingEnabled, name, mintFee, whitelistingEnabled } = mintingData;
   const [total, setTotal] = useState(0);
-  const price = Number(total) * Number(mintFee / 10**18);
-
+  const price = Number(total) * Number(mintFee / 10 ** 18);
 
   return (
     <div className="sidebar">
       <div className="mintSidebarMain">
         <img
-          src={"https://foxchangechachinglayer.s3.amazonaws.com/bucketFolder/1673008109763"}
+          src={
+            "https://foxchangechachinglayer.s3.amazonaws.com/bucketFolder/1673008109763"
+          }
           alt="fox"
         />
         <h3>{name}</h3>
         {mintingEnabled ? (
-          <p className={"mintStatus enabled"}>Enabled</p>
+          <p className={"mintStatus enabled"}>Minting Open</p>
         ) : (
-          <p className={"mintStatus closed"}>Closed</p>
+          <p className={"mintStatus closed"}>Minting Closed</p>
         )}
       </div>
-      <div>
-        <div className="whitelist whitelistOpen">
-          <p>✔ No whitelisting required</p>
+      {!whitelistingEnabled && (
+        <div>
+          No whitelisting required
+          <div className="whitelist whitelistOpen">
+            <p>✔ No whitelisting required</p>
+          </div>
+          <div className="mintPrice">
+            <p>{price} FX</p>
+            <p>{parseFloat((price * FX_PRICE).toFixed(4))} USD</p>
+          </div>
         </div>
-        <div className="mintPrice">
-          <p>{price} FX</p>
-          <p>{parseFloat((price * FX_PRICE).toFixed(4))} USD</p>
-        </div>
-      </div>
+      )}
+
       <MintCounter
         mintingData={mintingData}
         mintAction={mintAction}
