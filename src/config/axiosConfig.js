b7d6aa3e4adb70dca83ee1664a680a4a8138b90f
Mohammed El Gadi <melgadi.timwi@gmail.com>
2023-02-23 20:37:40 +0100

    added chainId

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,32 +1,36 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-axios.interceptors.request.use((config)=>{
-    return {
-        ...config,
-        'Access-Control-Allow-Origin': '*'
-    };
-})
+let reqInstance = axios.create({
+  headers: {
+    "X-CHAIN-ID" : "90001"
+  },
+});
 
+reqInstance.interceptors.request.use((config) => {
+  return {
+    ...config,
+    "Access-Control-Allow-Origin": "*",
+  };
+});
 
+reqInstance.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
 
-axios.interceptors.response.use(null, (error) => {
-    const expectedError =
-        error.response &&
-        error.response.status >= 400 &&
-        error.response.status < 500;
-
-    if (!expectedError) {
-        toast.error("An unexpected error occurred.");
-    }
-    return Promise.reject(error);
+  if (!expectedError) {
+    toast.error("An unexpected error occurred.");
+  }
+  return Promise.reject(error);
 });
 
 const methods = {
-    get: axios.get,
-    post: axios.post,
-    put: axios.put,
-    delete: axios.delete,
-}
+  get: reqInstance.get,
+  post: reqInstance.post,
+  put: reqInstance.put,
+  delete: reqInstance.delete,
+};
 
-export default methods ;
+export default methods;
