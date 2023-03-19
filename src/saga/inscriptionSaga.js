71d19aa5a86621b1e628a0ef251ff6b3221dd914
Mohammed El Gadi <melgadi.timwi@gmail.com>
2023-03-19 21:51:53 +0100

    update - add balance condition

--- src/saga/inscriptionSaga.js
+++ src/saga/inscriptionSaga.js
@@ -1,27 +1,44 @@
-import { toast } from 'react-toastify';
-import { call, takeLatest } from 'redux-saga/effects';
+import { toast } from "react-toastify";
+import { call, takeLatest } from "redux-saga/effects";
+import { sendInscription, getInscriptionBalance } from "../api/utilsApi.js";
 import { requestInscription } from "../services/bitCoinInscription.js";
-import { REQUEST_INSCRIPTION } from './actions.js';
+import { REQUEST_INSCRIPTION } from "./actions.js";
 import { signWallet } from "./userSaga";
 
 function* runRequestInscription(action) {
-    try {
-        const token = yield call(signWallet);
-        const { imageFile, fileSize, estimatedCost, receiverAddress } = action.payload;
-        const inscriptionRes = yield call(requestInscription, imageFile, fileSize, estimatedCost, receiverAddress, token);
-        action.onSuccess(inscriptionRes)
-    } catch (error) {
-        console.error(error);
-        toast.error("Error while requesting inscription");
-        action.onError(error);
+  try {
+    const token = yield call(signWallet);
+
+    //verify if the balance is alimented correctly for the requested file
+    const balanceInscription = yield call(getInscriptionBalance, token);
+
+    if (!balanceInscription?.isConditionFilled) {
+      toast.error("Please try the minting process later");
+      action.onError("Balance - Error while requesting inscription");
+      return;
     }
+
+    const { imageFile, fileSize, estimatedCost, receiverAddress } =
+      action.payload;
+    yield call(requestInscription, fileSize, estimatedCost);
+
+    const inscriptionRes = yield call(sendInscription, {
+      imageFile,
+      fileSize,
+      receiverAddress,
+      token,
+    });
+
+    action.onSuccess(inscriptionRes.data);
+  } catch (error) {
+    console.error(error);
+    toast.error("Error while requesting inscription");
+    action.onError(error);
+  }
 }
 
 function* requestInscriptionSaga() {
-    yield takeLatest(REQUEST_INSCRIPTION, runRequestInscription)
+  yield takeLatest(REQUEST_INSCRIPTION, runRequestInscription);
 }
 
-
-export {
-    requestInscriptionSaga
-};
+export { requestInscriptionSaga };
